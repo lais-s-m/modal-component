b6e271b65ebbb080cd0a2e4aafcbdfd1378c787d
Laís Bomtempo <95880678+lais-s-m@users.noreply.github.com>
2023-06-21 11:05:39 -0300

    Update useModal.js

--- src/_modalComponent/useModal.js
+++ src/_modalComponent/useModal.js
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 
-const useModal = (stateName = 'isOpen', functionName = 'toggle') => {
+const useModal = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
-  return { [`${stateName}`]: isOpen, [`${functionName}`]: toggle };
+  return [isOpen, toggle];
 };
 
 export default useModal;
